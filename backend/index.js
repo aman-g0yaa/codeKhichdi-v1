7a34fcae42871060c913d389d153fc571f4d840d
aman-g0yaa <amangoyal2339@gmail.com>
2024-09-10 23:09:32 +0530

    ADD: Created few end points

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.post("/register",(req,res)=>{
+    try {
+
+        //get all data from body
+
+        const {firstName, lastName, email, password} = req.body;
+
+        //validate the data 
+
+        if(!(firstName && lastName && email && password)){
+            throw new Error("Entered Details Invalid");
+        }
+
+        //check if user already exsist or not 
+
+
+
+
+        //encrpyting the password 
+        //if not register in db
+        //return response
+
+    } catch(error) {
+        console.error(error.message);
+        return res.status(400).json({
+            error: "Bad Request",
+            message: error.message
+        });
+    }
+});
+
+
+app.listen(5000,()=>{
+    console.log("Server Listening on port 5000!");
+});
